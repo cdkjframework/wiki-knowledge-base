27610ad5419eb846586a4b79d86d549559cb1c5c
xialin <xialin@lesmarthome.com>
2026-01-16 16:43:00 +0800

    增加请求工具

---
+++ knowledge-util/src/main/java/com/framewiki/knowledge/config/KnowledgeConfig.java
@@ -0,0 +1,10 @@
+package com.framewiki.knowledge.config;/**
+ * @ProjectName: knowledge-base
+ * @Package: com.framewiki.knowledge.config
+ * @ClassName: KnowledgeConfig
+ * @Description: java类作用描述
+ * @Author: xiaLin
+ * @Version: 1.0
+ */
+public class KnowledgeConfig {
+}
